e91b0ec7cee6fd4893d75dbc839b9c038ac928b6
freedom <862866850@qq.com>
2018-05-06 22:25:19 +0800

    mabtis 增删改查

--- src/com/itheima/MybatisTest/MybatisTest.java
+++ src/com/itheima/MybatisTest/MybatisTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by ningli on 2018/5/2.
@@ -25,7 +26,45 @@ public class MybatisTest {
             SqlSession sqlSession = sqlSessionFactory.openSession();
             Customer customer = sqlSession.selectOne("com.itheima.mapper" + ".CustomerMapper.findCustomerbyid",1);
             System.out.println(customer.toString());
+
+            List<Customer> customers = sqlSession.selectList("com.itheima.mapper" +".CustomerMapper.findCustomerbyName","j");
+            for (Customer customer1:customers) {
+                System.out.println("-----------Test-----------customer1值=" + customer1 + "," + "当前类=.()");
+            }
+
+            Customer customer2 =new Customer();
+            customer2.setUsername("rose");
+            customer2.setJobs("student");
+            customer2.setPhone("13333533091");
+//            int rows = sqlSession.insert("com.itheima.mapper" +".CustomerMapper.addCustomer",customer2);
+//            if (rows > 0) {
+//                System.out.println("-----------Test-----------值=" +  "插入成功"+ "," + "当前类=.()");
+//            }else {
+//                System.out.println("-----------Test-----------值=" + "插入失败" + "," + "当前类=.()");
+//            }
+//            sqlSession.commit();
+
+//            Customer customer3 = new Customer();
+//            customer3.setId(4);
+//            customer3.setUsername("rose");
+//            customer3.setJobs("programmer");
+//            customer3.setPhone("18579402223");
+//            int rows = sqlSession.update("com.itheima.mapper" +".CustomerMapper.updateCustomer",customer3);
+//            if (rows > 0) {
+//                System.out.println("-----------Test-----------值=" +  "修改成功"+ "," + "当前类=.()");
+//            }else {
+//                System.out.println("-----------Test-----------值=" + "修改失败" + "," + "当前类=.()");
+//            }
+
+            int rows = sqlSession.update("com.itheima.mapper" +".CustomerMapper.deleteCustomer",4);
+            if (rows > 0) {
+                System.out.println("-----------Test-----------值=" +  "删除成功"+ "," + "当前类=.()");
+            }else {
+                System.out.println("-----------Test-----------值=" + "删除失败" + "," + "当前类=.()");
+            }
+
             sqlSession.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -33,4 +72,6 @@ public class MybatisTest {
 
         }
     }
+
+
 }
